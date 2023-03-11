6fbcf1eaa530037dfdf4e01afc51c66564dcd290
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-03-11 12:53:10 +0530

    Update index.js

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,7 +2,7 @@ import Details from "./Details";
 import DubAnime from "./DubAnime"
 import OptionFetcher from "./genre";
 import Movie from "./movie"
-import Popular from "./movie"
+import Popular from "./Popular"
 import RecentAnime from "./RecentAnime"
 import Stream from "./Stream"
 import TopAnimeAiring from "./topAiring"
