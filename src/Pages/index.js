611f90b8cc75f24c7710d48598e4a4e53d76bce0
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-03-11 11:48:57 +0530

    Add files via upload

---
+++ src/Pages/index.js
@@ -0,0 +1,19 @@
+import Details from "./Details";
+import DubAnime from "./DubAnime"
+import OptionFetcher from "./genre";
+import Movie from "./movie"
+import Popular from "./movie"
+import RecentAnime from "./RecentAnime"
+import Stream from "./Stream"
+import TopAnimeAiring from "./topAiring"
+
+export{
+    Details,
+    DubAnime,
+    OptionFetcher,
+    Movie,
+    Popular,
+    RecentAnime,
+    Stream,
+    TopAnimeAiring
+}
