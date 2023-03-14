5d3e724412d328910728d70124500ab35802ee61
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-03-14 22:42:36 +0530

    Add files via upload

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-// const openInNewTab = url => {
-//   window.open(url, '_blank', 'noopener,noreferrer')
-// }
+
 function Footer() {
   return (
     <>
@@ -12,6 +10,9 @@ function Footer() {
             <h3><span>Anime</span><span className="about-blue">Trix</span></h3>
             <p>AnimeTrix is not affiliated with or endorsed by any of the anime studios behind the creation of the anime presented on this site. This website is only an user interface presenting/linking various self-hosted files across the internet by other third-party providers for easy access. AnimeTrix never downloads the video from any source provider, link will be returned from the response hence it is completely not subjected to DMCA compliant.</p>
           </div>
+        <div className="join-social">
+        <a href="https://github.com/ShivaBhattacharjee/AnimeTrix/" target={"_blank"}><ion-icon name="logo-github" ></ion-icon></a>
+        </div>
         </div>
       </div>
     </>
