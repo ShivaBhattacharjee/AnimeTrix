64ebcc169be43ea4b6998098cf370725745c972d
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-03-28 00:28:49 +0530

    Added dummy comment box frontend
    idk when backend will be done till then avoid this pesky container

--- src/Pages/Stream.jsx
+++ src/Pages/Stream.jsx
@@ -3,7 +3,10 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { Link, useLocation, useParams } from "react-router-dom";
-import {Footer} from "../Components/";
+import { Footer } from "../Components/";
+import ReplyIcon from '@mui/icons-material/Reply';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 
 export default function Stream(props) {
   const { episodeId } = useParams();
@@ -29,7 +32,9 @@ export default function Stream(props) {
 
     const getDetail = async () => {
       const Detail = await axios
-        .get(`https://gogoanime-api-dc2c.up.railway.app/anime-details/${animeId}`)
+        .get(
+          `https://gogoanime-api-dc2c.up.railway.app/anime-details/${animeId}`
+        )
         .catch((err) => console.log("Connection Error"));
 
       const temp = episodeId;
@@ -42,7 +47,7 @@ export default function Stream(props) {
         title: Detail.data.animeTitle,
         url: window.location.pathname,
         animeId: animeId,
-        coverimg: Detail.data.animeImg
+        coverimg: Detail.data.animeImg,
       });
 
       setDetail(Detail.data);
@@ -55,7 +60,12 @@ export default function Stream(props) {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(lastwatch));
   }, [lastwatch]);
 
+  // reply logic
+  const [showReplyTextArea, setShowReplyTextArea] = useState(false)
 
+  const handleReplyClick = () => {
+    setShowReplyTextArea(!showReplyTextArea)
+  }
   return (
     <>
       <Helmet>
@@ -76,14 +86,16 @@ export default function Stream(props) {
         <>
           <div className="stream">
             <div className="stream-container">
-
               <div className="video-title">
                 <span>{detail.animeTitle}</span>
-                <p>Note :- Refresh the page if the player doesnt load (server except Vidstreaming might contain ads use an adblocker to block ads)</p>
+                <p>
+                  Note :- Refresh the page if the player doesnt load (server
+                  except Vidstreaming might contain ads use an adblocker to
+                  block ads)
+                </p>
               </div>
 
               <div className="video-player-list">
-
                 {/* Video Player */}
                 <div className="video-player">
                   <iframe
@@ -109,7 +121,9 @@ export default function Stream(props) {
                             key={ep.episodeNum}
                           >
                             {ep.episodeId === episodeId ? (
-                              <button className="active">{ep.episodeNum}</button>
+                              <button className="active">
+                                {ep.episodeNum}
+                              </button>
                             ) : ep.episodeNum % 2 === 0 ? (
                               <button>{ep.episodeNum}</button>
                             ) : (
@@ -120,13 +134,245 @@ export default function Stream(props) {
                   </div>
                 </div>
               </div>
+
+              {/* discussion */}
+              <div className="comments">
+                <div className="comments-header">
+                  <h3>Comments</h3>
+                </div>
+
+                <div className="comment-section">
+                  <div className="send-comment">
+                    <textarea
+                      name=""
+                      id=""
+                      placeholder="Leave a comment"
+                    ></textarea>
+                    <button>Comment</button>
+                  </div>
+
+                  <div className="comment-field">
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-image" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Guts</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Architecto exercitationem dicta quas quasi
+                            repellendus, voluptatem deserunt perferendis quam
+                            dolore molestiae quis commodi beatae accusantium
+                            minima veniam quibusdam, consequatur cupiditate
+                            aliquid?
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button onClick={handleReplyClick}><ReplyIcon /></button>
+                          <button className="like-active"><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+
+                        <div className={showReplyTextArea ?  "reply-textarea" : 'hide' }>
+                          <textarea name="" id="" placeholder="Leave a comment"></textarea>
+                          <button>Send Comment</button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-img" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Girffith</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Architecto exercitationem dicta quas quasi
+                            repellendus, voluptatem deserunt perferendis quam
+                            dolore molestiae quis commodi beatae accusantium
+                            minima veniam quibusdam, consequatur cupiditate
+                            aliquid?
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button  onClick={handleReplyClick}><ReplyIcon /></button>
+                          <button><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-img" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Casca</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Architecto exercitationem dicta quas quasi
+                            repellendus, voluptatem deserunt perferendis quam
+                            dolore molestiae quis commodi beatae accusantium
+                            minima veniam quibusdam, consequatur cupiditate
+                            aliquid?
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button><ReplyIcon /></button>
+                          <button><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-img" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Casca</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Testing comment
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button  onClick={handleReplyClick}><ReplyIcon /></button>
+                          <button><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-img" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Casca</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Comment section has no backend if you are wondering?
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button><ReplyIcon /></button>
+                          <button><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-img" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Casca</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Bro skill issues
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button><ReplyIcon /></button>
+                          <button><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-img" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Casca</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Big as paragraph cause why not?
+
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, exercitationem tempora ipsum atque nihil consequuntur, ad nulla voluptatem rem, voluptas mollitia eius! Iure, pariatur! Eaque, libero! Quidem incidunt obcaecati veritatis?
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button><ReplyIcon /></button>
+                          <button><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="user-comment">
+                      <div className="user-img">
+                        <img src="https://i.pinimg.com/originals/b8/bf/ac/b8bfac2f45bdc9bfd3ac5d08be6e7de8.jpg" alt="user-img" />
+                      </div>
+                      <div className="user-name-time-text">
+                        <div className="user-name-time">
+                          <span className="user-name">Casca</span>
+                          <span>12:00 AM</span>
+                        </div>
+                        <div className="user-text">
+                          <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Architecto exercitationem dicta quas quasi
+                            repellendus, voluptatem deserunt perferendis quam
+                            dolore molestiae quis commodi beatae accusantium
+                            minima veniam quibusdam, consequatur cupiditate
+                            aliquid?
+                          </p>
+                        </div>
+                        <div className="reply-like-replies">
+                          <button><ReplyIcon /></button>
+                          <button><ThumbUpIcon /></button>
+                          <button><ThumbDownIcon /></button>
+                          <button> 10 Replies</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <Footer/>
+          <Footer />
         </>
-
       ) : (
-
         <div className="spinner-box">
           <div className="configure-border-1">
             <div className="configure-core"></div>
@@ -135,9 +381,7 @@ export default function Stream(props) {
             <div className="configure-core"></div>
           </div>
         </div>
-
-      )
-      }
+      )}
     </>
   );
 }
