a1b090e2392fd986e6e43603b6d0f8ceced799fa
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-03-28 01:29:12 +0530

    updated register classes

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,46 +4,53 @@ import { Link } from 'react-router-dom'
 
 function Register() {
   return (
-    <div className="register">
-    <div className="register-container">
-      <h1>Sign Up</h1>
-      <form autoComplete="false">
-        <div className="form-group">
-        <input
-            type="text"
-            className="register-group-input"
-            placeholder="Username"
-            required
-          />
-          <input
-            type="text"
-            className="register-group-input"
-            placeholder="Email"
-            required
-          />
-          <input
-            type="password"
-            className="register-group-input"
-            placeholder="Password"
-            required
-          />
-           <input
-            type="password"
-            className="register-group-input"
-            placeholder="Confirm password"
-            required
-          />
-          <button type="button" className="register-sign-in">
-            Get OTP
-          </button>
-        </div>
-      </form>
+    <div className="login">
+      <div className="login-container">
+        <h1>Sign Up</h1>
+        <form autoComplete="off">
+          <div className="form-group">
+            <input
+              type="text"
+              className="login-group-input"
+              placeholder="Username"
+              required
+            />
+            <input
+              type="text"
+              className="login-group-input"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              className="login-group-input"
+              placeholder="Password"
+              required
+            />
+            <input
+              type="password"
+              className="login-group-input"
+              placeholder="Confirm password"
+              required
+            />
+            <input
+              type="password"
+              className="login-group-input"
+              placeholder="Enter Otp"
+              required
+            />
+            <button type="button" className="login-sign-in">
+              Get OTP
+            </button>
+          </div>
+        </form>
         <div className="login-here">
-          <span className='font'>Already have an account ?</span>
-          <Link to={'/'}>Log In</Link>
+          <Link to={'/login'}>
+            <span className='font'>Already have an account ?</span>
+            Log In</Link>
         </div>
+      </div>
     </div>
-  </div>
   )
 }
 
