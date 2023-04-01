3f2b0d99adfde4d49a376c44de7a422dbc3d08a9
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-04-01 14:38:50 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,57 +1,35 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const NewSeason = () => {
-    const [season, setSeason] = useState([])
-    const getSeason = async (id = 1) => {
-        const api = await fetch(`https://animetrix-api.onrender.com/new-season?page=${id}`)
-        setSeason(await api.json())
-    }
-    useEffect(() => {
-        getSeason()
-    }, []);
-
-    return (
-        <section className="movies">
-            <div className="filter-bar">
-                <div className="heading">
-                    <h3>New Seasons</h3>
-                </div>
+export default function Card(props) {
+  return (
+    <>
+      <div
+        className="movie-card" onClick={() => props.handelClick()}>
+        <div className="card-head">
+          <div className="bookmark-icon">
+            <i class="fa-solid fa-bookmark"></i>
+          </div>
+          <Link to={`/anime-details/${props.rec.animeId}`}>
+            <img
+              src={props.rec.animeImg}
+              alt={props.rec.animeId}
+              className="card-img"
+            />
+          </Link>
+          <div className="card-details">
+            <div className="episode-total">
+              <span>{(props.rec.subOrDub)}</span>
+              <span>{(props.rec.episodeNum)}</span>
             </div>
-            <div className="seasons-grid">
-                {season.map((newSeason, newSeasonId) => {
-                    return (
-                        <>
-                            <div className='season-card' key={newSeasonId.animeId}>
-
-                                <div className="season-head">
-                                    <div className="bookmark-icon">
-                                        <i class="fa-solid fa-bookmark"></i>
-                                    </div>
-                                    <Link to={`/anime-details/${newSeason.animeId}`}>
-                                        <img
-                                            src={newSeason.animeImg}
-                                            alt={newSeason.animeId}
-                                            className="season-img"
-                                        />
-                                    </Link>
-                                    <div className="season-details">
-                                        <div className="release-date-season">
-                                            <span className='season-relase'>{(newSeason.releasedDate)}</span>
-                                        </div>
-                                        <h5 className="season-title">{(newSeason.animeTitle)}</h5>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </>
-                    )
-                })}
-            </div>
-        </section>
-    )
+            <h5 className="card-title">{(props.rec.animeTitle)}</h5>
+            {props.ep !== "false" ? (
+              <div className="card-info">
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default NewSeason
