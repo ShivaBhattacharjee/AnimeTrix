55b751b64cea81c19e81fcc374f769f327d233a1
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-04-09 12:05:12 +0530

    Delete server directory

--- server/config/emailConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: 'textify80@gmail.com',
-    pass: process.env.EMAIL_PASSWORD || "nacatwkwalqpnbzk",
-  },
-};
