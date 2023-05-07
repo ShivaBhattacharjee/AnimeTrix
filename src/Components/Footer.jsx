1d06aaf6b5a501ec2e7855e683fb8c152fe8c769
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-05-07 12:08:15 +0530

    Footer  changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,26 +1,40 @@
 import React from "react";
-import ThemeSelector from "../Pages/ThemeSelector";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Footer() {
-  return (
-    <>
-      <div className="footer">
-      <ThemeSelector/>
-        <div className="footer-content">
 
-          <div className="footer-about">
-            <h3><span>Anime</span><span className="about-blue">Trix</span></h3>
-            <p>AnimeTrix is not affiliated with or endorsed by any of the anime studios behind the creation of the anime presented on this site. This website is only an user interface presenting/linking various self-hosted files across the internet by other third-party providers for easy access. AnimeTrix never downloads the video from any source provider, link will be returned from the response hence it is completely not subjected to DMCA compliant.</p>
-          </div>
-        <div className="join-social">
-        <a href="https://t.me/animetrix013" target="_blank"><i class="fa-brands fa-telegram"></i></a>
-        </div>
-        <div className="join-social">
-        <a href="https://instagram.com/animetrix.200" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-        </div>
-        </div>
+  const githubUrl = () => {
+    window.open('https://github.com/ShivaBhattacharjee/AnimeTrix')
+  }
+
+  const instagramUrl = () => {
+    window.open('https://www.instagram.com/animetrix.200/')
+  }
+  
+  return (
+    <div className="footer">
+      <h1>
+        <span className="white">Anime</span>Trix
+      </h1>
+      <p className="footer-about">
+        AnimeTrix is not affiliated with or endorsed by any of the anime studios
+        behind the creation of the anime presented on this site. This website is
+        only an user interface presenting/linking various self-hosted files
+        across the internet by other third-party providers for easy access.
+        AnimeTrix never downloads the video from any source provider, link will
+        be returned from the response hence it is completely not subjected to
+        DMCA compliant.
+      </p>
+      <div className="footer-social">
+        <button onClick={() => instagramUrl()}>
+          <InstagramIcon />
+        </button>
+        <button onClick={() => githubUrl()}>
+          <GitHubIcon />
+        </button>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 export default Footer;
