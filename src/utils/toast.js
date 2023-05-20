6e8fe0821ffc057ff380c04abe64c2c2237b81f2
Shiva <95211406+ShivaBhattacharjee@users.noreply.github.com>
2023-05-20 12:36:09 +0530

    Update toast.js

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -3,7 +3,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export const showErrorToast = (errorMessage) => {
     toast.error(errorMessage, {
         position: "top-center",
-        autoClose: 2000,
+        autoClose: 1000,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: true,
@@ -16,7 +16,7 @@ export const showErrorToast = (errorMessage) => {
   export const showSuccessToast = (successMessage) => {
     toast.success(successMessage, {
         position: "top-center",
-        autoClose: 2000,
+        autoClose: 1000,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: true,
