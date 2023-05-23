c4d9326b7f0bc6e793a0edb757499c311862b684
Shiva <shivaxwarrior@gmail.com>
2023-05-24 00:28:24 +0530

    changed api in stream

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -1,4 +1,3 @@
 export const HomeApi = "https://animetrix-api.vercel.app"
 export const ServerApi = "https://animetrix-login-backend.vercel.app/api/v1"
-// only for emergency and its slow asf
-export const StreamApi = "https://animetrix-api.onrender.com"
+export const StreamApi = "https://api.amvstr.ml"
